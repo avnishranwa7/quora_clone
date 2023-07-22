1188f355dcdc169ddf538c413486ebb1dc8bcfe0
Avnish Ranwa <avnishranwa7@gmail.com>
2023-07-23 02:25:28 +0530

    Set env variables

--- quora_clone/settings.py
+++ quora_clone/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-ndx_8@6j$s3**=si8sd&k+k-1x%e2c-$^(*o-pc4qk!o)vvq-8"
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
